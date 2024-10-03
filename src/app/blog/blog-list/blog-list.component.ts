07d75eb02be93f0c79999e31cb864f1db02704f1
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-10-03 22:20:49 +0200

    some change

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostService } from '../../core/post.service';
+import { BlogPost } from '../../models/blog-post.model';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
-  styleUrl: './blog-list.component.css',
+  styleUrls: ['./blog-list.component.css'],
 })
-export class BlogListComponent {}
+export class BlogListComponent implements OnInit {
+  trendingPosts$: Observable<BlogPost[]> | null = null;
+  nonTrendingPosts$: Observable<BlogPost[]> | null = null;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    // Fetch all posts
+    this.postService.getPosts().subscribe((posts) => {
+      // Separate trending and non-trending posts
+      const trending = posts.filter((post) => post.isTrending);
+      const nonTrending = posts.filter((post) => !post.isTrending);
+
+      this.trendingPosts$ = new Observable((observer) => {
+        observer.next(trending);
+      });
+
+      this.nonTrendingPosts$ = new Observable((observer) => {
+        observer.next(nonTrending);
+      });
+    });
+  }
+}
