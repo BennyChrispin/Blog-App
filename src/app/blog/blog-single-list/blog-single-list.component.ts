26d4a0342ddf1b6768a11984815b2208ed61258d
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-10-04 12:48:37 +0200

    some change

--- src/app/blog/blog-single-list/blog-single-list.component.ts
+++ src/app/blog/blog-single-list/blog-single-list.component.ts
@@ -9,12 +9,13 @@ import { BlogPost } from '../../models/blog-post.model';
   styleUrls: ['./blog-single-list.component.css'],
 })
 export class BlogSingleListComponent implements OnInit {
+  @Output() closeModal = new EventEmitter<void>();
   @Input() post: BlogPost | null = null;
   isOpen = false;
   blogId: string | null = null;
   loading: boolean = false;
   newCommentText: string = '';
-  @Output() closeModal = new EventEmitter<void>();
+  isExpanded: boolean = false;
 
   isHeartSolid = false;
 
@@ -49,63 +50,23 @@ export class BlogSingleListComponent implements OnInit {
 
   editPost() {
     if (this.post) {
-      // Implement your logic for editing the post
-      // You might want to navigate to an edit page or open an edit modal
       console.log('Editing post:', this.post);
     }
   }
 
-  deletePost() {
-    // if (this.post) {
-    //   // Implement the logic to delete the post using PostService
-    //   this.postService
-    //     .deletePost(this.post.id)
-    //     .then(() => {
-    //       console.log('Post deleted successfully');
-    //       this.close(); // Optionally close the modal after deletion
-    //     })
-    //     .catch((error: any) => {
-    //       console.error('Error deleting post:', error);
-    //     });
-    // }
-  }
+  deletePost() {}
 
-  likePost() {
-    // if (this.post && this.post.authorUUID) {
-    //   this.postService
-    //     .likePost(this.post.id, this.post.authorUUID)
-    //     .then(() => {
-    //       console.log('Post liked successfully');
-    //       // Optionally refresh the post to update likes count
-    //     })
-    //     .catch((error) => {
-    //       console.error('Error liking post:', error);
-    //     });
-    // }
-  }
+  likePost() {}
 
-  addComment() {
-    // if (this.post && this.newCommentText) {
-    //   const newComment = {
-    //     author: 'Current User', // Replace with actual current user's name
-    //     text: this.newCommentText,
-    //   };
-    //   this.postService
-    //     .addComment(this.post.id, newComment)
-    //     .then(() => {
-    //       console.log('Comment added successfully');
-    //       this.newCommentText = ''; // Clear the input after adding
-    //       this.loadBlogDetail(this.post.id); // Refresh post to show new comment
-    //     })
-    //     .catch((error) => {
-    //       console.error('Error adding comment:', error);
-    //     });
-    // }
-  }
+  addComment() {}
 
   // Toggle functions for the buttons
   toggleHeart() {
     this.isHeartSolid = !this.isHeartSolid;
+  }
+  // Toggle the expanded state
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
   }
 
   open(post: BlogPost) {
