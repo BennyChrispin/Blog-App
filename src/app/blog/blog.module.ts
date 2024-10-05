887b1925292a08f77b8717c80ac93b09cf22f1ed
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-10-06 00:01:41 +0200

    some change

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogListComponent } from './blog-list/blog-list.component';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-import { BlogEditComponent } from './blog-edit/blog-edit.component';
-import { CommentSectionComponent } from './comment-section/comment-section.component';
 import { BannerComponent } from './banner/banner.component';
 import { BlogSingleListComponent } from './blog-single-list/blog-single-list.component';
 import { SharedModule } from '../shared/shared.module';
@@ -13,9 +10,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     BlogListComponent,
-    BlogDetailComponent,
-    BlogEditComponent,
-    CommentSectionComponent,
     BannerComponent,
     BlogSingleListComponent,
     ConfirmationModalComponent,
