47e5064a3f874c4167d850fe4028d18e111c0b7e
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-10-06 22:26:36 +0200

    some change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,12 @@ import { bookmarkReducer } from '../store/blog/blog.reducer';
     EffectsModule.forRoot([AuthEffects]),
     StoreModule.forRoot({ auth: authReducer, bookmarks: bookmarkReducer }),
     AngularFireAuthModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [provideClientHydration(), appConfig.providers],
   bootstrap: [AppComponent],
